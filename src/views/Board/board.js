23d885f0c9f38adbb536da7325f062c23527a20f
eduardogarcot <eduardogarcot@gmail.com>
2022-07-06 23:36:45 -0400

    feature: td-08 add request with temporary state

--- src/views/Board/board.js
+++ src/views/Board/board.js
@@ -1,6 +1,7 @@
 // IMPORTS
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BoardPanel from 'components/boardPanel';
+import http from 'services/http';
 
 // CLASSNAMES & CONSTANT
 const CONTAINER_CN = 'flex flex-row w-full justify-around';
@@ -8,11 +9,23 @@ const CONTAINER_CN = 'flex flex-row w-full justify-around';
 // COMPONENT
 const Board = () => {
   // REQUEST PARA COMPONENTES
+  const [task, setTasks] = useState({TO_DO:[],IN_PROGRESS:[],DONE:[]}); 
+  useEffect(()=>{
+    http.get('/tasks')
+      .then((response)=>{
+        const {data} = response;
+        setTasks(data);
+      })
+      .catch((error)=>{
+        console.log(error);
+        console.log("se murioooooooo");
+      })
+  },[])
   return (<>
     <div className={CONTAINER_CN}>
-      <BoardPanel name='TO DO' />
-      <BoardPanel name='IN PROGRESS' cards={[]} />
-      <BoardPanel name='DONE' cards={[]} />
+      <BoardPanel name='TO DO' cards={task.TO_DO} />
+      <BoardPanel name='IN PROGRESS' cards={task.IN_PROGRESS} />
+      <BoardPanel name='DONE' cards={task.DONE} />
     </div>
   </>
   );
