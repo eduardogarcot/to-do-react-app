9abc5e2d0e0f62d0e763b33e7c1650a9ba5ff86c
eduardogarcot <eduardogarcot@gmail.com>
2022-07-07 02:18:21 -0400

    feature: td-08 adding request and use redux state instead of local state

---
+++ src/utils/strHelper.js
@@ -0,0 +1,7 @@
+export const cleanStrWithUnderscore = str => str.replace('_',' ');
+
+const stringHelper = {
+  cleanStrWithUnderscore,
+}
+
+export default stringHelper;
--- src/views/Board/board.js
+++ src/views/Board/board.js
@@ -1,31 +1,37 @@
 // IMPORTS
-import React, { useEffect, useState } from 'react';
+import React, { useEffect} from 'react';
 import BoardPanel from 'components/boardPanel';
 import http from 'services/http';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectToDoList, pushToDo} from 'redux/slices/todoList';
+import stringHelper from 'utils/strHelper';
 
 // CLASSNAMES & CONSTANT
 const CONTAINER_CN = 'flex flex-row w-full justify-around';
 
 // COMPONENT
 const Board = () => {
-  // REQUEST PARA COMPONENTES
-  const [task, setTasks] = useState({TO_DO:[],IN_PROGRESS:[],DONE:[]}); 
+  const dispatch = useDispatch();
+  const tasksLists = useSelector(selectToDoList);
+  
   useEffect(()=>{
     http.get('/tasks')
       .then((response)=>{
         const {data} = response;
-        setTasks(data);
+        dispatch(pushToDo(data));
       })
       .catch((error)=>{
         console.log(error);
         console.log("se murioooooooo");
       })
-  },[])
+  },[dispatch])
   return (<>
     <div className={CONTAINER_CN}>
-      <BoardPanel name='TO DO' cards={task.TO_DO} />
-      <BoardPanel name='IN PROGRESS' cards={task.IN_PROGRESS} />
-      <BoardPanel name='DONE' cards={task.DONE} />
+      {Object.keys(tasksLists).map(status => {
+        const name = stringHelper.cleanStrWithUnderscore(status);
+        const cards = tasksLists[status];
+        return <BoardPanel name={name} cards={cards}/>
+      })}
     </div>
   </>
   );
