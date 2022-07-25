8ee647acb3d179ffbe874a2ab67e41b9e8375d9d
eduardogarcot <eduardogarcot@gmail.com>
2022-07-25 02:39:38 -0400

    feat: td-10 add handler to get projects

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3,6 +3,7 @@ import auth from 'services/auth'
 import apiServices from 'services/fakeServices';
 
 export const handlers = [
+  // Login Request
   rest.post('/login', (req, res, ctx) => {
     const {username, password} = req.body;
     const response = auth.fakeLoginService(username,password);
@@ -12,6 +13,7 @@ export const handlers = [
     )
     else return res(ctx.status(401));
   }),
+  // Request for Obtain all Task related to a specific Project
   rest.get('/tasks', (req, res, ctx) => {
     const {authorization} = req.headers._headers;
     if (authorization !== 'asdaS13rfnalsdKVdfh13fdvaeriAsDFA213Q') return res(ctx.status(401));
@@ -22,5 +24,17 @@ export const handlers = [
         ctx.json(response)
       );
     }
-  })
+  }),
+  // Request to Obtain all Projects related to a specific User
+  rest.get('/projects', (req, res, ctx) => {
+    const {authorization} = req.headers._headers;
+    if (authorization !== 'asdaS13rfnalsdKVdfh13fdvaeriAsDFA213Q') return res(ctx.status(401));
+    else {
+      const response = apiServices.getProjects();
+      return res(
+        ctx.status(200),
+        ctx.json(response)
+      );
+    }
+  }),
 ]
