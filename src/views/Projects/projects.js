813fd2ee834dcee3e9c403e1dc12e0fbb92e739a
eduardogarcot <eduardogarcot@gmail.com>
2022-07-25 02:45:05 -0400

    feat: td-10 add new service get projects in use with local state

--- src/views/Projects/projects.js
+++ src/views/Projects/projects.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import ProjectsPanel from 'components/projectsPanel';
+import http from 'services/http';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Projects = () => {
-  const projects = [
+  const [projects,setProjects] = useState([]);
+  const dispatch = useDispatch();
+  
+  useEffect(()=>{
+    http.get('/projects')
+      .then((response)=>{
+        const {data} = response;
+        setProjects(data);
+      })
+      .catch((error)=>{
+        console.log(error);
+        console.log("se murioooooooo");
+      })
+  },[dispatch])
+  /*const projects = [
     {
         id: '0001',
         title: 'Todo Website',
@@ -18,7 +34,8 @@ const Projects = () => {
         title: 'Todo Website',
         description: 'Create a website like JIRA, but with less features',
     },
-];
+];*/
+
   return ( 
     <>
       <ProjectsPanel projects={projects}/>
