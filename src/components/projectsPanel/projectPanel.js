4c55e783a2bc97e9e743409f381a350d8c1b124c
eduardogarcot <eduardogarcot@gmail.com>
2022-07-26 01:03:01 -0400

    feat: td-10 add behavior to change project

--- src/components/projectsPanel/projectPanel.js
+++ src/components/projectsPanel/projectPanel.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Typography } from '@mui/material';
 import ProjectItem from './projectItem/projectItem';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { removeProjectById } from 'redux/slices/projects';
+import { selectCurrentProject, setCurrentProject } from 'redux/slices/currentProject';
+import http from 'services/http';
 
 const ProjectsPanel = ({projects}) => {
     const dispatch = useDispatch();
-    
+    const currentProject = useSelector(selectCurrentProject);
+
     const handleEditProject = (id) => {
         console.log("EDITED", id);
     }
@@ -15,8 +18,16 @@ const ProjectsPanel = ({projects}) => {
     }
     const handleSelectProject = (id) => {
         console.log("Selected", id);
+        const url = `/project/${id}`;
+        http.get(url)
+            .then((response)=>{
+                dispatch(setCurrentProject(response.data));
+            })
+            .catch(error=>{
+                console.log(error);
+            })
     }
-    const checkProject = (id) => id === 'a0001'; 
+    const checkProject = (id) => id === currentProject.id; 
     return ( 
     <div className='flex flex-col m-4 items-center shadow-xl border-2 border-slate-200'>
         <Typography variant="h5" component="p" className='w-full p-2 bg-slate-300'> 
