20c3f664d2d1a5023482317df6e77056fa26b40a
eduardogarcot <eduardogarcot@gmail.com>
2022-07-29 01:51:18 -0400

    feat: td-13 adding schema builder

--- src/utils/builders.js
+++ src/utils/builders.js
@@ -1,6 +1,26 @@
+import * as Yup from 'yup';
+
 export const ValuesBuilder = (values, config) => {
     const {name, value} = config;
     values[name]=value;
     return values;
+}
+
+export const SchemaBuilder = (schema, config) => {
+    const {name, options} = config;
+    switch (name) {
+        case 'title':
+            schema[name] = Yup.string().min(5, 'Too Short!').max(50, 'Too Long!').required('The Title is Required');
+            break;
+        case 'deadlineDate':
+            schema[name] = Yup.date().required('Required').min(new Date(), "Should be greater");
+            break;
+        case 'leaderName':
+            schema[name] = Yup.string().min(5, 'Too Short!').max(50, 'Too Long!').required('The TName of the Leader is Required');
+            break;
+        default:
+            break;
+    }
+    return schema;
 }
 
